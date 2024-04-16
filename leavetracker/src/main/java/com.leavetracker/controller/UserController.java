464f5647efd707de5e6c8ace3f0c73c542915ef9
prakyathk <prakyathkarkera4@gmail.com>
2024-04-16 16:22:03 +0530

    Completed Login Module

--- leavetracker/src/main/java/com.leavetracker/controller/UserController.java
+++ leavetracker/src/main/java/com.leavetracker/controller/UserController.java
@@ -13,43 +13,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class UserController {
 
-    //    @Autowired
-//    private UserService userService;
-//
-//    @PostMapping("/login")
-//    public ResponseEntity<String> login(@RequestBody User loginUser) {
-//        boolean isAuthenticated = userService.login(loginUser.getUsername(), loginUser.getPassword());
-//        if (isAuthenticated) {
-//            return new ResponseEntity<>("Login successful!", HttpStatus.OK);
-//        } else {
-//            return new ResponseEntity<>("Invalid credentials!", HttpStatus.UNAUTHORIZED);
-//        }
-//    }
-//
-//
-//
-//    @PostMapping("/create")
-//    public ResponseEntity<String> createUser(@RequestBody UserRequest userRequest) {
-//        try {
-//            userService.createUser (userRequest);
-//            return ResponseEntity.ok ("User created successfully.");
-//        } catch (Exception e) {
-//            return ResponseEntity.status (HttpStatus.INTERNAL_SERVER_ERROR).body ("Error creating user: " + e.getMessage ());
-//        }
-//    }
-//
-////
-////    @PutMapping("/{userId}/update-username")
-////    public void updateUsername(@PathVariable String userId, @RequestParam String userName) {
-////        userService.updateUsername(userId, userName);
-////    }
-////
-////    @PutMapping("/{userId}/update-password")
-////    public void updatePassword(@PathVariable String userId, @RequestParam String newPassword) {
-////        userService.updatePassword(userId, newPassword);
-////    }
-//}
-//
+
     @Autowired
     private UserService userService;
 
--- leavetracker/src/main/java/com.leavetracker/service/UserService.java
+++ leavetracker/src/main/java/com.leavetracker/service/UserService.java
@@ -14,58 +14,6 @@ import java.util.Optional;
 @Service
 public class UserService {
 
-//    @Autowired
-//    private UserRepository userRepository;
-//    @Autowired
-//    private BCryptPasswordEncoder passwordEncoder;
-//
-//    public boolean login(String userName, String password) {
-//        User user = userRepository.findByUsername(userName);
-//        if (user != null && passwordEncoder.matches(password, user.getPassword())) {
-//            return true;
-//        }
-//        return false;
-//    }
-//
-//    public void createUser(UserRequest userRequest) {
-//        User user = new User();
-//        user.setUserId(userRequest.getUserId());
-//        user.setUsername(userRequest.getUsername());
-//        user.setPassword(userRequest.getPassword());
-//        user.setDepartmentId(userRequest.getDepartmentId());
-//
-//        user.setPhoneNumber(userRequest.getPhoneNumber());
-//
-//
-//
-//
-//        String encryptedPassword = passwordEncoder.encode(user.getPassword());
-//        user.setPassword(encryptedPassword);
-//
-//        userRepository.save(user);
-//    }
-//
-////
-////    public void updateUsername(String userId, String username) {
-////        User user = userRepository.findById(userId).orElseThrow(() -> new UserNotFoundException ("User not found with id: " + userId));
-////        user.setUsername(username);
-////        userRepository.save(user);
-////    }
-////
-//////    public void updatePassword(String userId, String newPassword) {
-//////        User user = userRepository.findById(userId).orElseThrow(() -> new UserNotFoundException("User not found with id: " + userId));
-//////        String encryptedPassword = passwordEncoder.encode(newPassword);
-//////        user.setPassword(encryptedPassword);
-//////        userRepository.save(user);
-//////    }
-////public void updatePassword(String userId, String newPassword) {
-////    User user = userRepository.findById(userId).orElseThrow(() -> new UserNotFoundException("User not found with id: " + userId));
-////    String encryptedPassword = passwordEncoder.encode(newPassword);
-////    user.setPassword(encryptedPassword);
-////    userRepository.saveAndFlush (user);
-////}
-//
-//}
 @Autowired
 private UserRepository userRepository;
 
